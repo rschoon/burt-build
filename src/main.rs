b84d553324347f95237c12badb27736bfc06f165
Robin Schoonover <robin@cornhooves.org>
2025-02-28 21:38:21 -0700

    Add build subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::path::{Path, PathBuf};
 
 use anyhow::Context;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 mod builder;
 mod file;
@@ -16,17 +16,40 @@ fn read_burt_file(path: &Path) -> anyhow::Result<file::RootSection> {
 
 #[derive(Parser)]
 struct Args {
+    /// source file
     #[clap(short, long, default_value="build.burt")]
-    file: PathBuf
+    file: PathBuf,
+
+    #[command(subcommand)]
+    command: Command
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Build {
+        targets: Vec<String>,
+    }
+}
+
+fn build_targets(burtfile: file::RootSection, targets: Vec<String>) -> anyhow::Result<()> {
+    for target in targets {
+        if target.starts_with("+") {
+            let mut build = builder::Build::new();
+            build.build(&burtfile, &target[1..])?;
+        } else {
+            anyhow::bail!("Unknown target {}", target);
+        }
+    }
+
+    Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     let burtfile = read_burt_file(&args.file)?; 
-
-    let mut build = builder::Build::new();
-    build.build(&burtfile, "default")?;
-
-    Ok(())
+    match args.command {
+        Command::Build { targets } => build_targets(burtfile, targets)
+    }
 }
+

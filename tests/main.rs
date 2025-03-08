e4906d6adc0f7fa58c8d79f86690d85e96f6422f
Robin Schoonover <robin@rschoon.com>
2025-03-07 18:28:37 -0700

    Reorganize tests and add an export artifact test

--- tests/main.rs
+++ tests/main.rs
@@ -4,38 +4,41 @@ use predicates::prelude::*;
 use rstest::rstest;
 use serde::Deserialize;
 use tempfile::TempDir;
-use std::{io::{Read, Write}, path::PathBuf, process::Command};
+use std::{io::Read, path::PathBuf, process::Command};
 
 #[derive(Debug, Deserialize)]
 struct TestData {
     run: Vec<TestRun>
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Default, Deserialize)]
+#[serde(default)]
 struct TestRun {
     args: Vec<String>,
-    #[serde(default)]
-    status_code: i32
+    status_code: i32,
+    verify_files: Vec<PathBuf>,
 }
 
 #[rstest]
 fn main(
-    #[files("tests/data/**/*.txt")] path: PathBuf
+    #[files("tests/data/**/*.toml")] path: PathBuf
 ) {
-    let mut f = std::fs::File::open(path).unwrap();
-    let mut buffer = String::new();
-    f.read_to_string(&mut buffer).unwrap();
-
-    let pos = buffer.find("\n###\n").unwrap();
-    let toml_data = &buffer[..pos];
-    let burt_data = &buffer[pos+5..];
-
-    let test: TestData = toml::from_str(toml_data).unwrap();
+    let burt_name = {
+        let mut b = path.clone();
+        b.set_extension("burt");
+        b
+    };
+    let mut f = std::fs::File::open(&path).unwrap();
+    let mut toml_data = String::new();
+    f.read_to_string(&mut toml_data).unwrap();
+    let test: TestData = toml::from_str(&toml_data).unwrap();
 
     let temp_dir = TempDir::new().unwrap();
-    let burt_filename = temp_dir.path().join("build.burt");
-    let mut burt_file = std::fs::File::create(&burt_filename).unwrap();
-    burt_file.write(burt_data.as_bytes()).unwrap();
+    
+    if burt_name.exists() {
+        let burt_filename = temp_dir.path().join("build.burt");
+        std::fs::copy(burt_name, burt_filename).unwrap();
+    }
 
     if test.run.is_empty() {
         panic!("No test runs defined");
@@ -47,5 +50,12 @@ fn main(
         command.current_dir(temp_dir.path());
         let cmd_assert = command.assert();
         cmd_assert.code(predicate::eq(run.status_code));
+
+        for filename in &run.verify_files {
+            let filename = path.parent().unwrap().join(filename);
+            let result_file = temp_dir.path().join(filename.file_name().unwrap());
+            let predicate_file = predicate::path::eq_file(&filename);
+            assert!(predicate_file.eval(result_file.as_path()));
+        }
     }
 }
